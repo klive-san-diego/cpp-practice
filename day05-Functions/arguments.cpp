b8118b835b5f391e18c761832d294f64bf1f1070
klive-san-diego <klivesandiego2@gmail.com>
2026-03-09 21:35:18 +0800

    day05 - Practiced passing arguments to functions

--- day05-Functions/arguments.cpp
+++ day05-Functions/arguments.cpp
@@ -1,13 +1,15 @@
+// Topic: Parameters and Arguments
 #include <iostream>
 using namespace std; 
 
-    void greetUser(string name){
-        cout << "hi " << name << " nice to meet you!";
+    //Function that greets user by name
+    void greetUser(string name){ //Takes a string parameter called name
+        cout << "hi " << name << " nice to meet you!"; // Prints the greeting with the name
     }
 
     int main(){
-        greetUser("Klive");
-        return 0;
+        greetUser("Klive"); // Calls the function and passes Klive as the argument
+        return 0;// ends the program successfully
     }
     
 
